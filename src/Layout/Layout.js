5b32f7c89f31fb5000ccd66a06468d96966319ba
Steven <svramirez20@gmail.com>
2021-09-12 21:18:22 -0400

    added div

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+//this file is "./src/Layout/index.js" in Qualified
 
 function Layout() {
   return (
-    <>
+    <div>
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
         <NotFound />
       </div>
-    </>
+    </div>
   );
 }
 
