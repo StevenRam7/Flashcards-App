f41765d1657b697cf40772cc409d17bb717dc596
Steven <svramirez20@gmail.com>
2021-09-22 02:52:03 -0400

    buttons

--- src/Layout/ListDecks.js
+++ src/Layout/ListDecks.js
@@ -16,8 +16,12 @@ return (
     <div class="card w-75">
     <div class="card-body">
       <h5 class="card-title">{deck.name}</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="btn btn-primary">Button</a>
+      <p class="card-text">{deck.description}</p>
+      <div className="button-row">
+      <button class="btn btn-secondary"><span class="oi oi-eye">View</span></button>
+      <button class="btn btn-primary"><span class="oi oi-book">Study</span></button>
+      <button class="btn btn-danger"><span class="oi oi-trash"></span></button>
+      </div>
     </div>
   </div>
   )})}
